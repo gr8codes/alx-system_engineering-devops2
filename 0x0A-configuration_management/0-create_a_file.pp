884e8ab1288787787d99c937c0164db85743dbc4
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-08-25 12:34:39 +0100

    test: puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-
-file {
-  '/tmp/school':
-    ensure  => file,
-    path    => '/tmp/school',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
+# Crate a file in /tmp
+file { 'school':
+  ensure  => present,
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
