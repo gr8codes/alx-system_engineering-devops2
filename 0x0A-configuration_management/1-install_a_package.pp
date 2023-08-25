e0285f412a9768e4f393ef972ca98a6b72dd43f3
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-08-25 12:53:06 +0100

    Update 1-install_a_package.pp
    ensure pip3 is installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,10 @@
 # Install flask from pip3
 
-class { 'python::pip':
-  ensure   => present,  # ensure pip is installed
+package { 'pip3':
+  ensure => 'installed',
 }
 
-python::pip::package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-
 }
