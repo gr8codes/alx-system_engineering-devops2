32cb595fd6fe165d64af3a9271582b970368d12d
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-08-27 14:55:33 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,12 @@
-#!/usr/bin/env bash
-# Make changes to ssh configuration.
-file_line { 'Turn off password authentication':
-  ensure  => present,
-  path    => '/etc/ssh/sshd_config',
-  line    => '   PasswordAuthentication no',
-  replace => true,
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  ensure  => present,
-  path    => '/etc/ssh/sshd_config',
-  line    => '   IdentityFile ~/.ssh/school',
-  replace => true,
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
