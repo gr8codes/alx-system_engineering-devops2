7ef84b513ba474d64d78d22511d4cf3b2965876a
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-08-27 14:53:01 +0100

    Create 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Make changes to ssh configuration.
+file_line { 'Turn off password authentication':
+  ensure => present,
+  path   => '/etc/ssh/sshd_config',
+  line   => '   PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/sshd_config',
+  line   => '   IdentityFile ~/.ssh/school',
+}
