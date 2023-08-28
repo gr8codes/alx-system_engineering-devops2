27463c6e708a5c7625722b733c14fe3f5528a1a5
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-08-28 20:19:56 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,29 +1,23 @@
-# Install nginx
-
-exec {'Update system':
-  command => '/usr/bin/apt-get update',
+# Puppet manifest to install nginx
+package { 'nginx':
+  ensure => installed,
 }
 
-# Install nginx
-package {'nginx':
-  ensure  => installed,
-  require => Exec['Update system'],
-}
-
-# Write 'Hello World' to the main nginx page
-file {'/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
-# Redirect /redirect_me to a youtube video
-file {'Redirect me':
+# Nginx config
+file_line { 'redirect_me':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-service {'nginx':
+# Nginx config
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Nginx service
+service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
 }
