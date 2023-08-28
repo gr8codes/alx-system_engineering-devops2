dd9db8d70ca67cf110f07eb381043061c290b92a
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-08-28 19:11:16 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,20 +11,16 @@ package {'nginx':
 
 # Write 'Hello World' to the main nginx page
 file {'/usr/share/nginx/www/index.html':
-  ensure  => present,
   content => 'Hello World!',
-  require => Package['nginx'],
 }
 
 # Redirect /redirect_me to a youtube video
 file {'Redirect me':
   command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
   provider => 'shell',
-  require  => Package['nginx'],
 }
 
 service {'nginx':
   ensure  => running,
-  enable  => true,
   require => Package['nginx'],
 }
