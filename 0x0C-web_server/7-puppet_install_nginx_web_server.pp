c52e61c8ae10119c0a7e02223ec3faf252ba4f30
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-08-28 20:15:19 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,14 +11,16 @@ package {'nginx':
 }
 
 # Write 'Hello World' to the main nginx page
-file {'/usr/share/nginx/www/index.html':
+file {'/var/www/html/index.html':
   content => 'Hello World!',
 }
 
 # Redirect /redirect_me to a youtube video
 file {'Redirect me':
-  command  => 'sudo sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://linkedin.com/in/achrafelkhnissi/ permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell',
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service {'nginx':
