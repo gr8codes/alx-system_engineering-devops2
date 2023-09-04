15671d7f9d7a1dfda02d5ea30de8ce31c42408eb
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-09-04 17:06:45 +0100

    add: task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,35 @@
-# Puppet manifest to install nginx
+# Setup New Ubuntu server with nginx and custom HTTP header
+
+# Update system
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
+# Install nginx
 package { 'nginx':
-  ensure => installed,
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-# Nginx config
-file_line { 'redirect_me':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+# Add custom index.html
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-# Nginx config
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+# Redirect to my linkedin profile
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://linkedin.com/in/achrafelkhnissi permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-# Nginx config
+# Add custom HTTP header
 exec {'HTTP header':
 	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
-# Nginx service
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+# Start nginx service
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
